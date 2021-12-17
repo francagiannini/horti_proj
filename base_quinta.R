02bf2a1bcbf32b6ce24e678e43cff596919a5181
VicMarinelli <mariavictoriamarinelli@gmail.com>
2021-12-17 23:24:31 +0100

    17/12/2021

--- base_quinta.R
+++ base_quinta.R
@@ -7,7 +7,7 @@ library(googledrive)
 
 ### muestra ########
 drive_download(
-  "https://docs.google.com/spreadsheets/d/1mr8NAuNCDRg20TFOT69bobscBcu8RhfolD9qZOWsNFQ/edit#gid=1517732420",
+  "https://docs.google.com/spreadsheets/d/1Oxm_2O8KQpKgQKWDhB39I7Pbqdef2wYE/edit#gid=1608429386",
   path = "data/muestra.xlsx",
   overwrite = TRUE
 )
@@ -25,8 +25,6 @@ prod_tipo <- muestra %>% group_by(Codigo,tipo_hortaliza) %>%
               values_from = prod_media,
               names_prefix = 'prod_')
 
-
-
 num_tipo <- muestra %>% group_by(Codigo) %>% 
    #mutate(prod_media=mean(Peso_kg_m2_mensual)) %>% 
   mutate(no_rows = nlevels(as.factor(tipo_hortaliza))) %>% 
@@ -35,24 +33,17 @@ num_tipo <- muestra %>% group_by(Codigo) %>%
 
 
 sup_tipo <- muestra %>% group_by(Codigo,tipo_hortaliza) %>% 
-  mutate(sup_suma_tipo=sum(`% asignacion de sup hort quinta...25`)) %>% 
-  select(sup_suma_tipo) %>% unique() %>% 
+  mutate(sup_mean_tipo=mean(asignacion_sup_hort_quinta)) %>% 
+  select(sup_mean_tipo) %>% unique() %>% 
   pivot_wider(names_from = tipo_hortaliza, 
-              values_from = sup_suma_tipo,
+              values_from = sup_mean_tipo,
               names_prefix = 'sup_') 
 
-# supfrec_tipo <- muestra %>% group_by(Codigo,tipo_hortaliza) %>% 
-#   mutate(supfrec_suma_tipo=sum(`Sup asig x frec (%)...291`)) %>% 
-#   select(supfrec_suma_tipo) %>% unique() %>% 
-#   pivot_wider(names_from = tipo_hortaliza, 
-#               values_from = supfrec_suma_tipo,
-#               names_prefix = 'supfrec_') 
-
 rec_tipo <- muestra %>% group_by(Codigo,tipo_hortaliza) %>% 
-  mutate(rec_mean_tipo=mean(`recurrencia`)) %>% 
-  select(rec_mean_tipo) %>% unique() %>% 
+  mutate(rec_sum_tipo=sum(recurrencia)) %>% 
+  select(rec_sum_tipo) %>% unique() %>% 
   pivot_wider(names_from = tipo_hortaliza, 
-              values_from = rec_mean_tipo,
+              values_from = rec_sum_tipo,
               names_prefix = 'rec_') 
 
 rtas <- bind_cols(prod_global, 
@@ -75,6 +66,7 @@ base_quintas <- bind_cols(muestra20q,
   select(!starts_with("Motivo"))
 
 write.table(base_quintas, "base_quintas.txt", sep="\t")
+
 base_quintas_num<- base_quintas %>% select_if(is.numeric)
 
-write.table(base_quintas, "base_quintas_num.txt", sep="\t")
+write.table(base_quintas_num, "base_quintas_num.txt", sep="\t")
--- exploratoria.R
+++ exploratoria.R
@@ -1,84 +1,25 @@
-library(tidyverse)
+library(tidyselect)
+library(magrittr)
 library(corrplot)
 
 base_quintas <- read.table("base_quintas.txt", 
                            header = T, 
                            sep = "\t", 
                            na.strings = "NA")
+base_quintas[is.na(base_quintas)] <- 0
 
 ##### Variables continuas_CORRELACION ###########
 
 data_sel_num<- base_quintas %>% select_if(is.numeric)
 #colnames(data_sel_num)
 
-data_sel_num<- base_quintas %>% select("Quinta..Superficie.Total",                                  
-                                       "Quinta..Superficie.Hortícola",                              
-                                       "Quinta..Superficie.cubierta..ha.",                          
-                                       "Quinta..Superficie.con.invernaderos",                       
-                                       "Quinta..Superficie.con.malla.antigranizo",                  
-                                       "Quinta..Superficie.con.mediasombra",                        
-                                       "Intalaciones_nro",                                        
-                                       "Producción..Cantidad.de.especies",                      
-                                       "Agua.para.riego..Superficie...Gravedad",                    
-                                       "Agua.para.riego..Superficie...Goteo",                       
-                                       "Agua.para.riego..Superficie...Aspersión",                   
-                                       "Practicas.productivas_nro",                                 
-                                       "Productor.principal..Edad",                                 
-                                       "Familia.del.PP..Cantidad",                                  
-                                       "Familia.del.PP..Cantidad.varones",                          
-                                       "Familia.del.PP..Cantidad.vujeres",                          
-                                       "Familia.del.PP..Cantidad.argentinos.as",                    
-                                       "Familia.del.PP..Cantidad.bolivianos.as",                
-                                       "Medieria..Cantidad",                                        
-                                       "Medieria..Cantidad.varones",                                
-                                       "Medieria..Cantidad.mujeres",                                
-                                       "Medieria..Cantidad.argentinos",                             
-                                       "Medieria..Cantidad.bolivia",                                
-                                       "Peones..Cantidad",                                       
-                                       "Trabajo.extra.predial..sector.horticola",                   
-                                       "Trabajo.extra.predial..sector.agropecuario.no.hortícola",   
-                                       "Trabajo.extra.predial..sector.no.agropecuario",             
-                                       "Contrata.prepeación.de.la.tierra..Cantidad.de.días",        
-                                       "Contrata.prepeación.de.la.tierra..Cantidad.de.personas",    
-                                       "Siembra.y.riego..Cantidad.de.días",                         
-                                       "Siembra.y.riego..Cantidad.de.personas",                     
-                                       "Contrata.cosecha..acondic.y.embalaje..Cantidad.de.días",    
-                                       "Contrata.cosecha..acondic.y.embalaje..Cantidad.de.personas",
-                                       "Respondente..Experiencia.agrícola..años.",                  
-                                       "Respondente..Ingreso.a.quinta..años.",                      
-                                       "vur_2020_mean",                                             
-                                       "Abasto_distancia..m.",                                      
-                                       "SanMiguel_distancia..m.",                                   
-                                       "prod_media",                                                
-                                       "prod_hoja",                                                 
-                                       "prod_fruto",                                                
-                                       "prod_tallo_bulbo",                                          
-                                       "prod_tuberculo_raiz",                                       
-                                       "prod_inflo_col",                                            
-                                       "no_rows",                                                   
-                                       "sup_hoja",                                                  
-                                       "sup_fruto",                                                 
-                                       "sup_tallo_bulbo",                                           
-                                       "sup_tuberculo_raiz",                                        
-                                       "sup_inflo_col",                                             
-                                       "rec_hoja",                                                  
-                                       "rec_fruto",                                                 
-                                       "rec_tallo_bulbo",                                           
-                                       "rec_tuberculo_raiz",                                        
-                                       "rec_inflo_col"               
-)
-data_sel_num<- data_sel_num %>% select_if(is.numeric)
+data_sel_num<- data_sel_num %>% dplyr::select(!'acelga':'rabanito')
 
 corr <- cor(data_sel_num, use = "pairwise.complete.obs", method = "spearman")
 
-# corrplot(corr, type="upper", 
-#          order="alphabet", 
-#          #p.mat = p.mat, 
-#          tl.pos='n',
-#          #p.mat = corr,
-#          sig.level = 0.05)
-
-# p.mat <- cor.test(data_sel_num, method = "spearman") 
+corr <- as.data.frame(corr)  
+  
+write_excel_csv2(corr, "corr.xlsx", delim=';')
 
 
 corr0.4 <- as.data.frame(corr) %>% 
@@ -106,22 +47,11 @@ corr0.4 <- as.data.frame(corr) %>%
                   rowname== "rec_inflo_col")
 
 
-write_excel_csv2(corr0.4, "corr0.4.csv", delim=';')
+write_excel_csv2(corr0.4, "corr0.4.xlsx", delim=';')
 
 #########  VARIABLES CATEGORICAS #############
 
 data_sel_cat <- base_quintas %>% select_if(is.character) 
-#%>%
-#   select(!'Quinta..Instalaciones...Ninguna',
-#          'Producción.no.horticola..Cereales',
-#          'Producción.no.horticola..Forrajes',
-#          'Producción.no.horticola..Oleaginosas',
-#          'Agua.para.riego..Fuente...Agua.de.red',
-#          'Agua.para.riego..Fuente...Otra',
-#          'Herramientas..sembradora...Contrata',
-#          'Herramienta..Pulverizadora...Propiedad.compartida',
-#          'Herramienta..Pulverizadora...Contrata',
-#          )
 
 ##productividad media
 data_sel_cat$prod_media <- base_quintas$prod_media
@@ -141,7 +71,7 @@ ifelse(is.numeric(a), a, "NA")
 cbind(colnames(data_sel_cat)[i], a, a<0.05)
 
 }
-resanova <- lapply(2:10,anov)
+#resanova <- lapply(2:10,anov)
 
 resanova <- lapply(2:ncol(data_sel_cat)-1,anov)
 
@@ -185,9 +115,40 @@ write_excel_csv2(resanova, "resanova_hoja.csv", delim=";")
 
 resanova %>% filter(p< 0.1)
 
+##productividad POR TIPO: tallo_bulbo
+data_sel_cat <- base_quintas %>% select_if(is.character)
+data_sel_cat$prod_tallo_bulbo <- base_quintas$prod_tallo_bulbo
+
+anov <- function(i){
+  
+  colnames(data_sel_cat)[i]
+  
+  data_sel <- base_quintas %>% select(prod_tallo_bulbo, colnames(data_sel_cat)[i])
+  
+  try(anova <- anova(lm(formula = prod_tallo_bulbo~., data_sel)), TRUE)
+  
+  a <- anova$`Pr(>F)`[1]
+  
+  ifelse(is.numeric(a), a, "NA")
+  
+  cbind(colnames(data_sel_cat)[i], a, a<0.05)
+  
+}
+
+resanova <- lapply(2:ncol(data_sel_cat)-1,anov)
+
+resanova <- as.data.frame(do.call("rbind", resanova))
+
+colnames(resanova) <- c("variable","p","sign")
+
+write.table(resanova, "resanova_prod_tallo_bulbo.txt", sep="\t")
+write_excel_csv2(resanova, "resanova_prod_tallo_bulbo.csv", delim=";")
+resanova %>% filter(p< 0.1)
+
 ##productividad POR TIPO: fruto
 data_sel_cat <- base_quintas %>% select_if(is.character)
-data_sel_cat$prod_fruto <- base_quintas$prod_fruto
+data_sel_cat$prod_fruto <- base_quintas$prod_fruto  
+
 
 anov <- function(i){
   
@@ -213,36 +174,6 @@ colnames(resanova) <- c("variable","p","sign")
 
 write.table(resanova, "resanova_prod_fruto.txt", sep="\t")
 write_excel_csv2(resanova, "resanova_prod_fruto.csv", delim=";")
-resanova %>% filter(p< 0.1)
-
-##productividad POR TIPO: inflo_col
-data_sel_cat <- base_quintas %>% select_if(is.character)
-data_sel_cat$prod_inflo_col <- base_quintas$prod_inflo_col
-
-anov <- function(i){
-  
-  colnames(data_sel_cat)[i]
-  
-  data_sel <- base_quintas %>% select(prod_inflo_col, colnames(data_sel_cat)[i])
-  
-  try(anova <- anova(lm(formula = prod_inflo_col~., data_sel)), TRUE)
-  
-  a <- anova$`Pr(>F)`[1]
-  
-  ifelse(is.numeric(a), a, "NA")
-  
-  cbind(colnames(data_sel_cat)[i], a, a<0.05)
-  
-}
-
-resanova <- lapply(2:ncol(data_sel_cat)-1,anov)
-
-resanova <- as.data.frame(do.call("rbind", resanova))
-
-colnames(resanova) <- c("variable","p","sign")
-
-write.table(resanova, "resanova_prod_inflo_col.txt", sep="\t")
-write_excel_csv2(resanova, "resanova_prod_inflo_col.csv", delim = ";")
 resanova %>% filter(p< 0.1)
 
 ##productividad POR TIPO: tuberculo_raiz
@@ -275,17 +206,17 @@ write.table(resanova, "resanova_prod_tuberculo_raiz.txt", sep="\t")
 write_excel_csv2(resanova, "resanova_prod_tuberculo_raiz.csv", delim=";")
 resanova %>% filter(p< 0.1)
 
-##productividad POR TIPO: tallo_bulbo
+##productividad POR TIPO: inflo_col
 data_sel_cat <- base_quintas %>% select_if(is.character)
-data_sel_cat$prod_tallo_bulbo <- base_quintas$prod_tallo_bulbo
+data_sel_cat$prod_inflo_col <- base_quintas$prod_inflo_col
 
 anov <- function(i){
   
   colnames(data_sel_cat)[i]
   
-  data_sel <- base_quintas %>% select(prod_tallo_bulbo, colnames(data_sel_cat)[i])
+  data_sel <- base_quintas %>% select(prod_inflo_col, colnames(data_sel_cat)[i])
   
-  try(anova <- anova(lm(formula = prod_tallo_bulbo~., data_sel)), TRUE)
+  try(anova <- anova(lm(formula = prod_inflo_col~., data_sel)), TRUE)
   
   a <- anova$`Pr(>F)`[1]
   
@@ -301,9 +232,10 @@ resanova <- as.data.frame(do.call("rbind", resanova))
 
 colnames(resanova) <- c("variable","p","sign")
 
-write.table(resanova, "resanova_prod_tallo_bulbo.txt", sep="\t")
-write_excel_csv2(resanova, "resanova_prod_tallo_bulbo.csv", delim=";")
+write.table(resanova, "resanova_prod_inflo_col.txt", sep="\t")
+write_excel_csv2(resanova, "resanova_prod_inflo_col.csv", delim = ";")
 resanova %>% filter(p< 0.1)
+
 
 ####### SUPERFICIE por TIPO ########
 
@@ -610,3 +542,34 @@ colnames(resanova) <- c("variable","p","sign")
 write.table(resanova, "resanova_rec_tallo_bulbo.txt", sep="\t")
 write_excel_csv2(resanova, "resanova_rec_tallo_bulbo.csv", delim=";")
 resanova %>% filter(p< 0.1)
+
+##numero de tipos
+data_sel_cat <- base_quintas %>% select_if(is.character)
+data_sel_cat$no_rows<- base_quintas$no_rows
+
+anov <- function(i){
+  
+  colnames(data_sel_cat)[i]
+  
+  data_sel <- base_quintas %>% select(no_rows, colnames(data_sel_cat)[i])
+  
+  try(anova <- anova(lm(formula = no_rows~., data_sel)), TRUE)
+  
+  a <- anova$`Pr(>F)`[1]
+  
+  ifelse(is.numeric(a), a, "NA")
+  
+  cbind(colnames(data_sel_cat)[i], a, a<0.05)
+  
+}
+
+resanova <- lapply(2:ncol(data_sel_cat)-1,anov)
+
+resanova <- as.data.frame(do.call("rbind", resanova))
+
+colnames(resanova) <- c("variable","p","sign")
+
+write.table(resanova, "resanova_no_rows.txt", sep="\t")
+write_excel_csv2(resanova, "resanova_no_rows.csv", delim=";")
+resanova %>% filter(p< 0.1)
+
